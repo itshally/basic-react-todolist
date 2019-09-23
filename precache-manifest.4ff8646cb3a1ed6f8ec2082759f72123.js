9708c7b600cacd3d6a81f40362c03a5b14b0af27
HB <hbianan@gmail.com>
2019-09-23 10:47:46 -0400

    Updates

---
+++ precache-manifest.4ff8646cb3a1ed6f8ec2082759f72123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a516d7ed217c98b95c6824c6e274988c",
+    "url": "/itshally/basic-react-todolist/index.html"
+  },
+  {
+    "revision": "207947f9fa241b6b43bf",
+    "url": "/itshally/basic-react-todolist/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "78a3ece31591fd5f3a4a",
+    "url": "/itshally/basic-react-todolist/static/js/2.9aa0a6e2.chunk.js"
+  },
+  {
+    "revision": "207947f9fa241b6b43bf",
+    "url": "/itshally/basic-react-todolist/static/js/main.24591e0c.chunk.js"
+  },
+  {
+    "revision": "3a86f3e549740630a458",
+    "url": "/itshally/basic-react-todolist/static/js/runtime-main.43314668.js"
+  }
+]);
